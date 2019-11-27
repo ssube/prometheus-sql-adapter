eb96b894af5f4248ea08037da5bc7c8ebd0d0b4e
ssube <seansube@gmail.com>
2019-11-27 07:18:17 -0600

    feat(schema/views): add hourly load aggregate, example union query

--- schema/views/instance-load.sql
+++ schema/views/instance-load.sql
@@ -2,10 +2,26 @@ CREATE VIEW agg_instance_load
 WITH (
   timescaledb.continuous,
   timescaledb.refresh_interval = '5m',
-  timescaledb.refresh_lag = '5m')
-AS SELECT
+  timescaledb.refresh_lag = '5m'
+) AS SELECT
   lid,
   time_bucket('5 minutes', time) "bucket",
+  AVG(value) AS "avg_load",
+  MAX(value) AS "max_load"
+FROM metric_samples
+WHERE
+  name = 'node_load1'
+GROUP BY lid, bucket;
+
+
+CREATE VIEW agg_instance_load_long
+WITH (
+  timescaledb.continuous,
+  timescaledb.refresh_interval = '1h',
+  timescaledb.refresh_lag = '1h'
+) AS SELECT
+  lid,
+  time_bucket('1 hour', time) AS "bucket",
   AVG(value) AS "avg_load",
   MAX(value) AS "max_load"
 FROM metric_samples
