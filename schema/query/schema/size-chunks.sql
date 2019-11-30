a94e581c9d1a7abda1cea74de221f90687ff5b6a
ssube <seansube@gmail.com>
2019-11-30 11:21:42 -0600

    fix(query/schema): format chunk size

--- schema/query/schema/size-chunks.sql
+++ schema/query/schema/size-chunks.sql
@@ -1,7 +1,7 @@
 SELECT 
   MAX(table_name) AS metric,
   to_timestamp(lower(ranges[1])/10^6) AS time,
-  SUM(total_bytes)
+  pg_size_pretty(SUM(total_bytes)) AS value
 FROM _timescaledb_catalog.hypertable, chunk_relation_size(table_name::text) 
 WHERE 
   table_name LIKE 'metric%' 
