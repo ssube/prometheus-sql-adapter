2732bacdfba8efdb748c5f07f16344c4db4e6a17
ssube <seansube@gmail.com>
2019-11-30 06:17:41 -0600

    feat(benchmark): add metrics_values equivalent view, ensure value chunks are same size

--- benchmark/compat.sql
+++ benchmark/compat.sql
@@ -4,5 +4,12 @@ CREATE VIEW metrics_labels AS
 SELECT
   l.lid AS id,
   l.labels->>'__name__' AS metric_name,
-  labels
+  l.labels
 FROM metric_labels AS l;
+
+CREATE VIEW metrics_values AS
+SELECT
+  s.lid AS labels_id,
+  s.time,
+  s.value
+FROM metric_samples AS s;
--- benchmark/compress.sql
+++ benchmark/compress.sql
@@ -1,3 +1,5 @@
+SELECT set_chunk_time_interval('metrics_values', INTERVAL '1 hour');
+
 ALTER TABLE metrics_values SET (
   timescaledb.compress,
   timescaledb.compress_segmentby = 'labels_id'
