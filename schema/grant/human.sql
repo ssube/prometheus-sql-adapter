a9fc78cfc9f1b009b784e238c1ba68d732e1eac7
ssube <seansube@gmail.com>
2019-12-02 17:50:01 -0600

    fix(schema/grant): apply lowest statement timeout last

--- schema/grant/human.sql
+++ schema/grant/human.sql
@@ -1,6 +1,7 @@
-ALTER ROLE :role_name SET statement_timeout=30000;
+-- grant/grafana should also be run for this role
+\i schema/grant/grafana.sql
 
 GRANT SELECT ON metric_samples TO :role_name;
 
--- grant/grafana should also be run for this role
-\i schema/grant/grafana.sql
+-- grafana sets a 60s timeout, decrease that
+ALTER ROLE :role_name SET statement_timeout=30000;
