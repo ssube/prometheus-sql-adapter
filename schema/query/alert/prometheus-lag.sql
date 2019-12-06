3c8d205b957d375732eaf5b09578612ce1229796
ssube <seansube@gmail.com>
2019-12-06 07:36:41 -0600

    feat(schema/query): add prometheus lag and sample rate

---
+++ schema/query/alert/prometheus-lag.sql
@@ -0,0 +1,32 @@
+SELECT
+  metrics.labels->>'pod' AS "metric",
+  MAX("time") AS "time",
+  EXTRACT(EPOCH FROM NOW()) - MAX(value) AS "value"
+FROM metrics
+WHERE
+  time > NOW() - INTERVAL '15 minute' AND
+  name = 'prometheus_remote_storage_highest_timestamp_in_seconds' AND
+  value != 'NaN'
+GROUP BY metric
+ORDER BY time DESC
+LIMIT 4;
+
+
+SELECT
+  MAX(pod) AS "metric",
+  "time",
+  MAX("local") - MAX("remote") AS "value"
+FROM (
+  SELECT
+    metrics.labels->>'pod' AS "pod",
+    $__timeGroup("time", $__interval),
+    CASE WHEN name = 'prometheus_remote_storage_highest_timestamp_in_seconds' THEN value ELSE NULL END AS "local",
+    CASE WHEN name = 'prometheus_remote_storage_queue_highest_sent_timestamp_seconds' THEN value ELSE NULL END AS "remote"
+  FROM metrics
+  WHERE
+    $__timeFilter("time")
+    AND name IN ('prometheus_remote_storage_highest_timestamp_in_seconds', 'prometheus_remote_storage_queue_highest_sent_timestamp_seconds')
+    AND value != 'NaN'
+) AS t
+GROUP BY time, pod
+ORDER BY time, pod;
---
+++ schema/query/alert/prometheus-samples.sql
@@ -0,0 +1,51 @@
+SELECT
+  bucket AS "time",
+  metric,
+  ((
+  CASE 
+    WHEN lag(value) OVER w IS NULL THEN 0
+    WHEN value > lag(value) OVER w THEN (value - lag(value) OVER w)
+    ELSE value
+  END
+  ) / EXTRACT(EPOCH FROM INTERVAL '$__interval')) AS "value"
+FROM (
+SELECT
+  labels->>'pod' AS "metric",
+  time_bucket('$__interval', "time") AS "bucket",
+  MAX(value) AS "value"
+FROM metrics
+WHERE
+  $__timeFilter("time") and
+  name = 'prometheus_remote_storage_succeeded_samples_total'
+GROUP BY metric, bucket
+ORDER BY metric, bucket
+) AS m
+WHERE value > 0
+WINDOW w AS (PARTITION BY metric ORDER BY bucket)
+ORDER BY time;
+
+SELECT
+  bucket AS "time",
+  metric,
+  ((
+  CASE 
+    WHEN lag(value) OVER w IS NULL THEN 0
+    WHEN value > lag(value) OVER w THEN (value - lag(value) OVER w)
+    ELSE value
+  END
+  ) / EXTRACT(EPOCH FROM INTERVAL '$__interval')) AS "value"
+FROM (
+SELECT
+  labels->>'pod' AS "metric",
+  time_bucket('$__interval', "time") AS "bucket",
+  MAX(value) AS "value"
+FROM metrics
+WHERE
+  $__timeFilter("time") and
+  name = 'adapter_samples_sent_total'
+GROUP BY metric, bucket
+ORDER BY metric, bucket
+) AS m
+WHERE value > 0
+WINDOW w AS (PARTITION BY metric ORDER BY bucket)
+ORDER BY time;
