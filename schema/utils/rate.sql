b18687b99af80c4ebd6fcd539d975e784e182693
ssube <seansube@gmail.com>
2019-12-14 10:06:05 -0600

    lint: better names for rate function params

--- schema/utils/rate.sql
+++ schema/utils/rate.sql
@@ -1,11 +1,11 @@
 -- find the rate of change, accounting for resets
 -- usage: rate_diff(value, lag(value) OVER w)
-CREATE OR REPLACE FUNCTION rate_diff(a float, b float) RETURNS float
+CREATE OR REPLACE FUNCTION rate_diff(last_sample float, lag_sample float) RETURNS float
 AS $$
   SELECT CASE
-    WHEN a >= b THEN a - b      -- rising edge
-    WHEN b IS NULL THEN NULL    -- leading gap
-    ELSE a                      -- falling edge/reset
+    WHEN last_sample >= lag_sample THEN last_sample - lag_sample      -- rising edge
+    WHEN lag_sample IS NULL THEN NULL                                 -- leading gap
+    ELSE last_sample                                                  -- falling edge/reset
   END
 $$
 LANGUAGE SQL
@@ -15,11 +15,11 @@ RETURNS NULL ON NULL INPUT;
 -- find the rate of change, accounting for leading resets and 0s (but requires both lag and lead rows)
 -- using "Null value: connected" in Grafana has a similar effect without fetching an extra row
 -- usage: rate_smooth(value, lag(value) OVER w, lead(value) OVER w)
-CREATE OR REPLACE FUNCTION rate_smooth(a float, b float, c float) RETURNS float
+CREATE OR REPLACE FUNCTION rate_smooth(last_sample float, lag_sample float, lead_sample float) RETURNS float
 AS $$
   SELECT CASE
-    WHEN b IS NULL THEN rate_diff(c, a)
-    ELSE rate_diff(a, b)
+    WHEN lag_sample IS NULL THEN rate_diff(lead_sample, last_sample)
+    ELSE rate_diff(last_sample, lag_sample)
   END
 $$
 LANGUAGE SQL
@@ -28,18 +28,18 @@ CALLED ON NULL INPUT;
 
 -- find the rate of change over time (per second), accounting for resets
 -- usage: rate_time(value, lag(value) OVER w, '$__interval')
-CREATE OR REPLACE FUNCTION rate_time(a float, b float, t INTERVAL) RETURNS float
+CREATE OR REPLACE FUNCTION rate_time(last_sample float, lag_sample float, time_range INTERVAL) RETURNS float
 AS $$
-  SELECT rate_diff(a, b) / interval_seconds(t)
+  SELECT rate_diff(last_sample, lag_sample) / interval_seconds(time_range)
 $$
 LANGUAGE SQL
 STABLE
 RETURNS NULL ON NULL INPUT;
 
 -- usage: rate_time_smooth(value, lag(value) OVER w, lead(value) OVER w, '$__interval')
-CREATE OR REPLACE FUNCTION rate_time_smooth(a float, b float, c float, t INTERVAL) RETURNS float
+CREATE OR REPLACE FUNCTION rate_time_smooth(last_sample float, lag_sample float, lead_sample float, time_range INTERVAL) RETURNS float
 AS $$
-  SELECT rate_smooth(a, b, c) / interval_seconds(t)
+  SELECT rate_smooth(last_sample, lag_sample, lead_sample) / interval_seconds(time_range)
 $$
 LANGUAGE SQL
 STABLE
