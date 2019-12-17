f763c0eaafd2d204d36c1ced5b75521ff07a9412
ssube <seansube@gmail.com>
2019-12-17 08:54:23 -0600

    fix(test): cover negative and reset cases within rate_time

--- test/rate_time.sql
+++ test/rate_time.sql
@@ -1,7 +1,12 @@
 BEGIN;
-SELECT plan(1);
+SELECT plan(3);
 
+-- should handle simple rates
 SELECT is(rate_time(10, 0, '10 seconds'), 1.0::float, '0 -> 10 over 10s is 1');
+-- should handle negative values
+SELECT is(rate_time(60, -60, '2 minutes'), 1.0::float, '-60 -> 60 over 2m is 1');
+-- should handle counter resets
+SELECT is(rate_time(40, 900, '20 seconds'), 2.0::float, '900 -> reset -> 40 over 20s is 2');
 
 SELECT * FROM finish();
 ROLLBACK;
