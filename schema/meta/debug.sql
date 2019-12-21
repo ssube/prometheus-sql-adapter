339f60c6cfe69bc69d38a0abe3a71079736b0caa
ssube <seansube@gmail.com>
2019-12-21 11:57:54 -0600

    fix(schema/meta): add version info to debug report

--- schema/meta/debug.sql
+++ schema/meta/debug.sql
@@ -1,3 +1,3 @@
-SELECT * FROM prom_sql_size_total();
-SELECT * FROM prom_sql_size_samples();
+SELECT CONCAT(major, '.', minor, '.', patch) AS schema_version FROM prom_sql_version();
+SELECT * FROM prom_sql_size();
 SELECT * FROM prom_sql_compress_ratio();
