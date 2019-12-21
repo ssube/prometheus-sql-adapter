7e59c6dadc8fa0703a831dbceda8fbf740eb9c9e
ssube <seansube@gmail.com>
2019-12-21 15:01:49 -0600

    feat(schema/meta): include table details in debug report

--- schema/meta/debug.sql
+++ schema/meta/debug.sql
@@ -1,3 +1,7 @@
 SELECT CONCAT(major, '.', minor, '.', patch) AS schema_version FROM prom_sql_version();
 SELECT * FROM prom_sql_size();
 SELECT * FROM prom_sql_compress_ratio();
+
+\d+ metric_labels
+\d+ metric_samples
+\d+ metrics
