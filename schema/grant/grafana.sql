954b1e419ca97a6f99934e8b3c64b20673553ae0
Zachary Softich <zach.softich@wheniwork.com>
2020-02-10 15:00:22 -0600

    feat(schema/caag): apply sql and grants

--- schema/grant/grafana.sql
+++ schema/grant/grafana.sql
@@ -16,6 +16,8 @@ GRANT SELECT ON agg_container_mem TO :role_name;
 GRANT SELECT ON agg_grafana_alert TO :role_name;
 GRANT SELECT ON agg_grafana_alert_long TO :role_name;
 
+GRANT SELECT ON agg_pod_count TO :role_name;
+
 GRANT SELECT ON cat_container TO :role_name;
 GRANT SELECT ON cat_instance TO :role_name;
 GRANT SELECT ON cat_name TO :role_name;
